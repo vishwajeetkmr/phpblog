bac38fab02ff6953fb1f0f2eb7a871b36248d56a
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2017-04-03 21:38:08 +0530

    Initial Commit

---
+++ includes/sidebar.php
@@ -0,0 +1,15 @@
+        <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+          <div class="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </div>
+          <div class="sidebar-module">
+            <h4>Categories</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">PHP & MYSQL</a></li>
+              <li><a href="#">HTML & CSS</a></li>
+              <li><a href="#">Javascript & jQuery</a></li>
+        
+            </ol>
+          </div>
+        </div><!-- /.blog-sidebar -->
---
+++ libs/database.php
@@ -0,0 +1,67 @@
+<?php
+
+class database{
+    public $host = DB_HOST;
+    public $user = DB_USER;
+    public $pass = DB_PASS;
+    public $db_name = DB_NAME;
+    
+    public $link;
+
+    public function __construct(){
+    $this->connect();
+}
+
+
+private function connect(){
+    
+    $this->link = new mysqli($this->host,$this->user,$this->pass,$this->db_name);
+    
+}
+    
+    public function select($query){
+        
+        $result = $this->link->query($query);
+        
+        if($result->num_rows > 0){
+            return $result;
+        }else{
+            return false;
+        }
+        
+    }
+    
+    public function insert($query){
+        
+        $insert = $this->link->query($query);
+        
+        if($insert){
+            header('location: index.php?insert=Post Inserted...');
+        }else{
+            echo "Post didnot Insert...";
+        }
+        
+    }
+    
+    public function update($query){
+        
+        $update = $this->link->query($query);
+        
+        if($update){
+            header('location: index.php?insert=Post Updated...');
+        }else{
+            echo "Post didnot Updated...";
+        }
+    
+}
+    public function delete($query){
+        
+        $delete = $this->link->query($query);
+        
+        if($delete){
+            header('location: index.php?insert=Post Deleted...');
+        }else{
+            echo "Post didnot Delete...";
+        }
+}
+}
